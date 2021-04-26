e9a095acbefe9bede30a8e3ab591f78d582b5f64
Boruchigor <boruchigor@gmail.com>
2021-04-26 14:30:51 +0200

    ss

---
+++ app.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+
+export default function App(){
+  
+  
+    return (
+      <div>
+      <h1>Test</h1>
+      
+      <hr />
+     
+      </div>
+    );
+  
+}
+
+ReactDOM.render(<App />, document.getElementById('main'));
